a0c2b383172429146561c6f55174cd2e2829bad6
Anthony Cecconato <antho.cecc@gmail.com>
2021-09-25 13:10:03 +0200

    Undo console.log

--- app/controllers/posts.controller.js
+++ app/controllers/posts.controller.js
@@ -127,8 +127,6 @@ exports.getPostsFeed = async (req, res) => {
 
     const count = await feed.countPosts();
 
-    console.log(offset, limit); // todo fix
-
     const datas = await feed.getPosts({
       offset,
       limit,
